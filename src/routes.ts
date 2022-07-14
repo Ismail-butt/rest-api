d6e349915a1e4fdae73c8ccc5e2b049253df78e1
Ismail <i180516@nu.edu.pk>
2022-07-14 18:23:07 +0500

    Create Session endpoint

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,20 @@
 import { Express, Request, Response } from 'express'
+import { createUserSessionHandler } from './controller/session.controller'
 import { createUserHandler } from './controller/user.controller'
 import validateResource from './middleware/validateResource'
+import { createSessionSchema } from './schema/session.schema'
 import { createUserSchema } from './schema/user.schema'
 
 function routes(app: Express) {
   app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
 
   app.post('/api/users', validateResource(createUserSchema), createUserHandler)
+
+  app.post(
+    '/api/sessions',
+    validateResource(createSessionSchema),
+    createUserSessionHandler
+  )
 }
 
 export default routes
