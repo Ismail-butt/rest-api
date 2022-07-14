6f3695622e3b42a0b594ec4421fdce50d5c9e400
Ismail <i180516@nu.edu.pk>
2022-07-14 19:44:58 +0500

    Get Sessions endpoint

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,15 @@ import connect from './utils/connect'
 import logger from './utils/logger'
 import routes from './routes'
 
+import deserializeUser from './middleware/deserializeUser'
+
 const port = config.get<number>('port')
 
 const app = express()
 
 app.use(express.json())
+
+app.use(deserializeUser)
 
 app.listen(port, async () => {
   logger.info(`App is running at http://localhost:${port}`)
--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
 import config from 'config'
-import { createSession } from '../service/session.service'
+import { createSession, findSessions } from '../service/session.service'
 import { validatePassword } from '../service/user.service'
 import { signJwt } from '../utils/jwt.utils'
 
@@ -31,4 +31,12 @@ export async function createUserSessionHandler(req: Request, res: Response) {
   // return acess & refresh tokens
 
   return res.send({ accessToken, refreshToken })
+}
+
+export async function getUserSessionsHandler(req: Request, res: Response) {
+  const userId = res.locals.user._id
+
+  const sessions = await findSessions({ user: userId, valid: true })
+
+  return res.send(sessions)
 }
