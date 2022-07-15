8ecd039e7af33573a41ce4c1462627ccae378eee
Ismail <i180516@nu.edu.pk>
2022-07-15 19:33:29 +0500

    First API Test done!

---
+++ src/__tests__/product.test.ts
@@ -0,0 +1,11 @@
+import supertest from 'supertest'
+
+describe('product', () => {
+  describe('get product route', () => {
+    describe('given the product does not exist', () => {
+      it('should return a 404', () => {
+        expect(true).toBe(true)
+      })
+    })
+  })
+})
